2ab62f99e5c139daf62013d39f12fc28930b80ac
Mr Magpie <joe.marrone@outlook.com>
2021-03-10 21:22:35 -0600

    map.js error

---
+++ assets/map.js
@@ -0,0 +1,14 @@
+var myMap = L.map("map", {
+    center: [45,-93],
+    zoom: 13
+  });
+console.log(myMap);
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 60,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: api_key,
+}).addTo(myMap);
