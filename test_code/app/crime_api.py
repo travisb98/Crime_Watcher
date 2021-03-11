980210b93726f7f2662dd18ed68d171d0aa279dd
unknown <travisbostick@gmail.com>
2021-03-10 18:10:36 -0600

    working on date dependent api calls for app

---
+++ test_code/app/crime_api.py
@@ -0,0 +1,3 @@
+##### i'll put everything from crime_api_test.ipynb in here once it's ready
+##### I will import this file into app.py and run it at the appropriate time
+##### probably once for making the model and once for finding crimes in your area
